1799fb9b4efc1068ca5e9e0c071fdfa8a4fa1795
James Ringer <james@ihearttech.net>
2024-06-16 01:41:42 +0000

    style: format eslint-config index.mjs

--- tools/eslint/index.mjs
+++ tools/eslint/index.mjs
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      ecmaVersion: "latest",
-      sourceType: "module",
+    js.configs.recommended,
+    eslintConfigPrettier,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        }
     },
-  },
-  // This must go last!
-  eslintPluginPrettierRecommended,
+    // This must go last!
+    eslintPluginPrettierRecommended
 ];
