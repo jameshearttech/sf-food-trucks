e95dda35d2a5c540b0a2accaad3b3c0c41e2fe17
James Ringer <james@ihearttech.net>
2024-06-22 22:09:37 +0000

    feat: default to lint-staged check config in root project

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,2 +1,3 @@
-import baseConfig from 'lint-staged-config';
+// Default to check in root project, but provide config to fix, too.
+import { check as baseConfig } from 'lint-staged-config';
 export default baseConfig;
