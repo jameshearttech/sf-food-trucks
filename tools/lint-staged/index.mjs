a9569cf146625fee2f83c809e8caa2b5a252e219
James Ringer <james@ihearttech.net>
2024-06-22 21:08:53 +0000

    feat: add syncpack to lint-staged

--- tools/lint-staged/index.mjs
+++ tools/lint-staged/index.mjs
@@ -2,5 +2,6 @@ export default {
     // The comma-separate string must not contain spaces (e.g., "*.{js, cjs, mjs}").
     '*.{js,cjs,mjs}': ['eslint', 'prettier --check'],
     '*.{scss,css,json,yaml,yml,html}': ['prettier --check'],
-    '*.md': ['remark', 'prettier --check']
+    '*.md': ['remark', 'prettier --check'],
+    'package.json': ['syncpack lint']
 };
