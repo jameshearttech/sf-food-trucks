e687216a999189668cb9500bd9ff96d7810e0ce0
James Ringer <james@ihearttech.net>
2024-06-30 10:12:11 +0000

    feat: add jest recommended config to eslint despite peer dependency warning

--- tools/eslint/index.mjs
+++ tools/eslint/index.mjs
@@ -3,11 +3,16 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginJest from 'eslint-plugin-jest';
 
 export default tseslint.config(
     js.configs.recommended,
     ...tseslint.configs.recommended,
     eslintConfigPrettier,
+    {
+        files: ['test/**'],
+        ...eslintPluginJest.configs['flat/recommended']
+    },
     {
         languageOptions: {
             globals: {
